cde6454b3e83cd108c67afe5aa9167d0506fe1aa
javipisobeto <arq.javierriver@gmail.com>
2016-12-21 18:40:02 -0300

    countChar 3.04, a function that count how many times does a character appears on a phrase. Fixed missing bracket on both countBs() and countChar()

--- countBs.js
+++ countBs.js
@@ -1,1 +1,19 @@
-function countBs()
+function countBs(phrase){
+	var count = 0;
+	for (var i = 0; i < phrase.length; i++) {
+		if(phrase.charAt(i) === "B"){
+			count++;
+		}
+	}
+	return count;
+}
+
+function countChar(phrase, character){
+	var count = 0; 
+	for (var i = 0; i < phrase.length; i++) {
+		if(phrase.charAt(i) === character){
+			count++;
+		}
+	}
+	return count;
+}
