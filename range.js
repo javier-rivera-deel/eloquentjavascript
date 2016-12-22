458ddaa916882454399c3d8a24122efe8a8cc115
javipisobeto <arq.javierriver@gmail.com>
2016-12-22 16:24:01 -0300

    excercise 04.03: range() function modified to take negativeparameters, which returns a reverse array of the input

--- range.js
+++ range.js
@@ -1,12 +1,14 @@
-<<<<<<< HEAD
-function range(start, end){
-	var fullrange = [];
-	for (var i = start; i <= end ; i++) {
+function range(start, end, step){
+  var fullrange = [];
+  if(step < 0){
+    for (var i = end; i <= start ; i-=step) {
+		fullrange.unshift(i);
+	};
+	return fullrange;
+  }else{
+    for (var j = start; j <= end ; j+=step) {
 		fullrange.push(i);
 	};
 	return fullrange;
-=======
-function range(){
-	
->>>>>>> 5856722532a8534f3cfb7fb2467c94fa04fa8dc1
+  }
 }
