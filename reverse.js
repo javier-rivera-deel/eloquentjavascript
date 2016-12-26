ffeb49c07042f3e82f4c864a75ec0bf417638d9b
javipisobeto <arq.javierriver@gmail.com>
2016-12-26 13:41:00 -0300

    whitespaces cleared

--- reverse.js
+++ reverse.js
@@ -1,18 +1,14 @@
 function reverseArrayInPlace(array){
         var length = Math.floor(array.length / 2);
-        var lastPlace;
         var firstPlace;
         var lastCount = array.length - 1;
     for (var i = 0; i < length; i++) {
         firstPlace = array[i];
         lastPlace = array[lastCount];
-        
         array[i] = lastPlace;
         array[lastCount] = firstPlace;
-        
         lastCount--;
     };
-
 }
 
 function reverse(array){
