14b88c1b6dd7c677303bf6a259d6dbbd0a02910f
javipisobeto <arq.javierriver@gmail.com>
2016-12-28 17:25:53 -0300

    arrayToList() close to what's expected

--- creatingLists.js
+++ creatingLists.js
@@ -1,58 +1,20 @@
 function arrayToList(array){
-	list = {};
-	list.value = array[0];
-	for(var i=0;i<array.length;i++){
-		if(array[i+1]){
-			list.rest = {value:array[i+1]}
+	var index = 0;
+	var list = addItem(index);
+	
+	function addItem(index){
+		console.log("index "+index);
+		if(index < array.length){
+			return {value:array[index],rest:addItem(index++)}
 		}
-		else{
-			list.rest = null;
+		else if(index == array.length){
+			return {value:array[index],rest:null};
 		}
-	}
-	console.log(list);
-}
-
-function arrayToList(array, count){
-	if(list){
-		if(array[++count]){/*
-			list.rest = {value:array[i+1]}*/
-		}
-		else{
-			list.rest = null;
-			return list;
-		}
-	}else{
-			var list = {
-				value : array[0]
-			}
-			count++;
-			while(count < array.length){
-					return arrayToList(array,count);
-			}
 			
 	}
+	return list;
 }
 
-
-var myArray = ["a","b","c"];
-arrayToList(myArray);
-//var newList = arrayToList(myArray);
-//console.log(newList);
-
-
-function listToArray(list){
-	
-}
-
-/*
-var newArray = arrayToList(myList);
-console.log(newArray);
-*/
-
-function prepend(){
-	
-}
-
-function nth(){
-	
-}
+var arr = [1,2,3];
+var lista = arrayToList(arr);
+console.log(lista);
