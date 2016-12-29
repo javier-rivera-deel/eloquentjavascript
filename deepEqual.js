7741f46f41ea5c62e71a3e8416052d97fc781987
javipisobeto <arq.javierriver@gmail.com>
2016-12-29 18:07:17 -0300

    deepEqual templated added tests

--- deepEqual.js
+++ deepEqual.js
@@ -1,14 +1,29 @@
-function deepEqual(){
-	
+function deepEqual(objectA, objectB){
+	console.log
 }
-
-
 
 
 //object for testing
 var obj = {
-	here:{
-		is:"an"
-	},
+	here:{is:"an"},
 	object:2
-}
+};
+var otherObj = {
+	here: 1,
+	object: 2
+};
+
+console.log(obj.here);
+
+//testing deepEqual
+//console.log(deepEqual(obj, obj));
+// → true
+//status: not tested
+
+//console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+//status: not tested
+
+//console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
+//status: not tested
