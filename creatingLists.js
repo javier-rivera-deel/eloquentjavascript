7396f1c483ad8d8b6361f0e7a7c52a6a4e2e70a0
javipisobeto <arq.javierriver@gmail.com>
2016-12-29 11:20:16 -0300

    arrayToList(array) created. A function that takes an array and creates and linked list.

--- creatingLists.js
+++ creatingLists.js
@@ -5,16 +5,15 @@ function arrayToList(array){
 	function addItem(index){
 		console.log("index "+index);
 		if(index < array.length){
-			return {value:array[index],rest:addItem(index++)}
+			return {value:array[index],rest:addItem(++index)}
 		}
-		else if(index == array.length){
-			return {value:array[index],rest:null};
+		else if(index ===array.length){
+			return null;
 		}
 			
 	}
 	return list;
 }
-
 var arr = [1,2,3];
 var lista = arrayToList(arr);
 console.log(lista);
